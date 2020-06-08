250a3f1da3f3e212c6359a71a55be336bb05af7e
chanakya <chanakya.prathi@gmail.com>
2020-06-08 23:02:15 +0530

    nav tabs content

--- src/app/home/components/nav-tabs/nav-tabs.component.ts
+++ src/app/home/components/nav-tabs/nav-tabs.component.ts
@@ -16,33 +16,33 @@ export class NavTabsComponent implements OnInit {
   navList: any[] = [
     {
       id: 'tab-software-development',
-      title: 'Software Development',
-      sub_title:"Choose Your Software Development Service",
-      desc:"Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
+      title: 'OPEX',
+      sub_title:"Arka11 will own the Installation along with O&M for next 25 years.",
+      desc:"Customer only has to pay for solar electricity at the rate lower by 20-40% than grid tariff. All that customer needs to do is to sign a power purchase agreement and sit back and enjoy.",
       active: true,
-      chkList: ['Advance Advisory Team','Professional Consulting Services','24/7 Support Help Center','Customer Service & Operations','There are many variations of passages.']
+      chkList: ['Zero Investment','Zero Performance Risk','Savings on the monthly electricity bills','Fixed Electricity price for next 25 years']
     },
     {
       id: 'tab-application-services',
-      title: 'Application Services',
-      sub_title:"Choose Your Application Service",
-      desc:"Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
-      chkList: ['Advance Advisory Team','Professional Consulting Services','24/7 Support Help Center','Customer Service & Operations','There are many variations of passages.']
+      title: 'RESCO',
+      sub_title:"Third party investor (RESCO) will own the installation",
+      desc:"Arka11 will carry the O&M for next 25 year for the RESCO. Customer will pay a pre-decided monthly tariff which is lower than prevailing electricity bill.",
+      chkList: ['No upfront Investment','Zero Performance Risk','Savings on the monthly electricity bills','Cheaper Electricity for next 25 years']
     },
     {
       id: 'tab-data-analytics',
-      title: 'Data Analytics',
-      sub_title:"Choose Your Data Analytics Service",
-      desc:"Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
-      chkList: ['Advance Advisory Team','Professional Consulting Services','24/7 Support Help Center','Customer Service & Operations','There are many variations of passages.']
+      title: 'CAPEX',
+      sub_title:"Customer will own the solar power plant installation",
+      desc:"Arka11 will do the design and plant construction and will take up O&M on regular basis.",
+      chkList: ['Savings on the monthly electricity bills','Cheaper Electricity for next 25 years','Best in Market Investment with Guaranteed ROI','Quicker Payback Period','Free Electricity after Payback period']
     },
-    {
-      id: 'tab-help-desk-services',
-      title: 'Help Desk Services',
-      sub_title:"Choose Your Help Desk Service",
-      desc:"Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
-      chkList: ['Advance Advisory Team','Professional Consulting Services','24/7 Support Help Center','Customer Service & Operations','There are many variations of passages.']
-    }
+    // {
+    //   id: 'tab-help-desk-services',
+    //   title: 'Help Desk Services',
+    //   sub_title:"Choose Your Help Desk Service",
+    //   desc:"Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
+    //   chkList: ['Advance Advisory Team','Professional Consulting Services','24/7 Support Help Center','Customer Service & Operations','There are many variations of passages.']
+    // }
   ];
 
 
